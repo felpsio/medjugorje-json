bc52a5be404610f7f3933f2fcbe5592b074c9e76
Felipe César <email@felipecesar.io>
2024-03-13 15:38:05 -0500

    bump

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.medjugorjeMessages = void 0;
+var messages_json_1 = __importDefault(require("./messages.json"));
+// Export the messages with the correct type
+exports.medjugorjeMessages = messages_json_1.default;
